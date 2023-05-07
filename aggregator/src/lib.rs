b9cf2a08df74593df0c2610a49cb1c6114c0b3bc
Troy Tabrilla <troy.tabrilla+git@gmail.com>
2023-05-07 15:27:36 -0700

    Update aggregator.

--- aggregator/src/lib.rs
+++ aggregator/src/lib.rs
@@ -7,30 +7,23 @@ pub mod sources;
 pub use sources::*;
 
 pub struct Aggregator {
-    sources: Vec<sources::Sources>,
+    anilist_api: AniListAPI,
 }
 
 impl Default for Aggregator {
     fn default() -> Aggregator {
         let anilist_api = AniListAPI::from("config/anilist_api.yaml");
-        let sources = vec![Sources::AniListAPI(anilist_api)];
 
-        Aggregator { sources }
+        Aggregator { anilist_api }
     }
 }
 
 impl Aggregator {
     pub async fn aggregate(&self) -> Result<(), Box<dyn Error>> {
-        for source in &self.sources {
-            match source {
-                Sources::AniListAPI(anilist_api) => {
-                    let lists = anilist_api.aggregate().await?;
-                    // @todo Save to mongodb
-                    // @todo Set up Docker
-                    println!("{:#?}", lists.anime);
-                }
-            }
-        }
+        let data = self.anilist_api.aggregate().await?;
+        // @todo Save to mongodb
+        // @todo Set up Docker
+        println!("{:#?}", data);
 
         Ok(())
     }
