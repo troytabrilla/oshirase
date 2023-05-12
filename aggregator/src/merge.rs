1912725a2756663cf76e4c1db5dd93e1cf61b3db
Troy Tabrilla <troy.tabrilla+git@gmail.com>
2023-05-11 23:59:52 -0700

    Flip logic for merge to improve results.

--- aggregator/src/merge.rs
+++ aggregator/src/merge.rs
@@ -3,10 +3,11 @@ use crate::sources::subsplease_scraper::AnimeScheduleEntry;
 use crate::CustomError;
 use crate::Result;
 
+use regex::Regex;
+use tantivy::DocAddress;
 use tantivy::ReloadPolicy;
 use tantivy::{collector::TopDocs, query::QueryParser, schema::*, Index};
 use tempfile::TempDir;
-
 pub struct Merge;
 
 impl Merge {
@@ -17,9 +18,8 @@ impl Merge {
         let index_path = TempDir::new()?;
 
         let mut schema_builder = Schema::builder();
-        schema_builder.add_text_field("media_index", STRING | STORED);
+        schema_builder.add_text_field("schedule_index", STRING | STORED);
         schema_builder.add_text_field("title", TEXT | STORED);
-        schema_builder.add_text_field("alt_title", TEXT | STORED);
 
         let schema = schema_builder.build();
 
@@ -27,27 +27,14 @@ impl Merge {
 
         let mut index_writer = index.writer(50_000_000)?;
 
-        let media_index: Field = Self::get_field(&schema, "media_index")?;
+        let schedule_index = Self::get_field(&schema, "schedule_index")?;
         let title = Self::get_field(&schema, "title")?;
-        let alt_title = Self::get_field(&schema, "alt_title")?;
 
-        for (i, entry) in (*media).iter_mut().enumerate() {
-            if let Some(status) = &entry.status {
-                if status != "CURRENT" {
-                    continue;
-                }
-            }
+        for (i, entry) in (*schedules).iter().enumerate() {
             let mut document = Document::default();
-            document.add_text(media_index, i);
-            if let Some(media_title) = &entry.title {
-                document.add_text(title, media_title);
-            }
-            if let Some(media_alt_title) = &entry.alt_title {
-                document.add_text(alt_title, media_alt_title);
-            }
-            if !document.is_empty() {
-                index_writer.add_document(document)?;
-            }
+            document.add_text(schedule_index, i);
+            document.add_text(title, &entry.title);
+            index_writer.add_document(document)?;
         }
 
         index_writer.commit()?;
@@ -60,35 +47,98 @@ impl Merge {
 
         let schema = index.schema();
         let title = Self::get_field(&schema, "title")?;
-        let alt_title = Self::get_field(&schema, "alt_title")?;
 
-        let query_parser = QueryParser::for_index(&index, vec![title, alt_title]);
+        let query_parser = QueryParser::for_index(&index, vec![title]);
 
-        for schedule in schedules {
-            let query = query_parser.parse_query(&schedule.title.replace('-', ""));
+        for entry in &mut *media {
+            if let Some(status) = &entry.status {
+                if status != "CURRENT" {
+                    continue;
+                }
+            }
 
-            let query = match query {
-                Ok(query) => query,
-                Err(err) => {
-                    println!("Could not parse query: {}", err);
+            let replacement = Regex::new(r"[-:]")?;
+
+            let query_title = match &entry.title {
+                Some(title) => {
+                    let title = replacement.replace_all(title, "");
+                    match query_parser.parse_query(title.to_string().as_str()) {
+                        Ok(query) => query,
+                        Err(err) => {
+                            println!("Could not parse title query: {}", err);
+                            continue;
+                        }
+                    }
+                }
+                None => {
+                    println!("No title to search for.");
                     continue;
                 }
             };
 
-            let top_docs = searcher.search(&query, &TopDocs::with_limit(1))?;
-
-            if let Some((_score, doc_address)) = top_docs.first() {
-                let doc = searcher.doc(*doc_address)?;
-                let field = schema.get_field("media_index");
-                if let Some(field) = field {
-                    let media_index = doc
-                        .get_first(field)
-                        .ok_or(CustomError::boxed("Could not get media index."))?;
-                    if let Value::Str(media_index) = media_index {
-                        let media_index = media_index.parse::<usize>()?;
-                        media[media_index].schedule = Some(schedule.clone());
+            let query_alt_title = match &entry.alt_title {
+                Some(alt_title) => {
+                    let alt_title = replacement.replace_all(alt_title, "");
+                    match query_parser.parse_query(alt_title.to_string().as_str()) {
+                        Ok(query) => query,
+                        Err(err) => {
+                            println!("Could not parse title query: {}", err);
+                            continue;
+                        }
                     }
                 }
+                None => {
+                    println!("No alt title to search for.");
+                    continue;
+                }
+            };
+
+            let top_docs_title = searcher.search(&query_title, &TopDocs::with_limit(1))?;
+            let top_docs_alt_title = searcher.search(&query_alt_title, &TopDocs::with_limit(1))?;
+
+            let title_doc = top_docs_title.first();
+            let alt_title_doc = top_docs_alt_title.first();
+
+            let get_doc_score_tuple =
+                |doc: Option<&(f32, DocAddress)>| -> Result<(f32, Option<AnimeScheduleEntry>)> {
+                    if let Some((score, doc_address)) = doc {
+                        let doc = searcher.doc(*doc_address)?;
+
+                        let field = schema.get_field("schedule_index");
+                        let schedule = if let Some(field) = field {
+                            let schedule_index = doc
+                                .get_first(field)
+                                .ok_or(CustomError::boxed("Could not get media index."))?;
+                            if let Value::Str(schedule_index) = schedule_index {
+                                let schedule_index = schedule_index.parse::<usize>()?;
+                                let schedule = &schedules[schedule_index];
+                                Some(schedule)
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        };
+
+                        Ok((*score, schedule.cloned()))
+                    } else {
+                        Ok((0.0, None))
+                    }
+                };
+
+            let title_score_tuple = get_doc_score_tuple(title_doc)?;
+            let alt_title_score_tuple = get_doc_score_tuple(alt_title_doc)?;
+
+            if title_score_tuple.1.is_some() && alt_title_score_tuple.1.is_some() {
+                if title_score_tuple.0 > alt_title_score_tuple.0 {
+                    entry.schedule = title_score_tuple.1;
+                } else {
+                    entry.schedule = alt_title_score_tuple.1;
+                }
+            } else if title_score_tuple.1.is_some() {
+                entry.schedule = title_score_tuple.1;
+            } else if alt_title_score_tuple.1.is_some() {
+                entry.schedule = alt_title_score_tuple.1;
             }
         }
 
